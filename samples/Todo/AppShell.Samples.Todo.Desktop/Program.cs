baedf62e5f6de6ab69a84850aa77e3eeb066551b
Christian Schwarz <christian.schwarz@refactory.at>
2015-04-24 19:11:23 +0200

    Initial todo sample app

---
+++ samples/Todo/AppShell.Samples.Todo.Desktop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppShell.Samples.Todo
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            TodoApp app = new TodoApp();
+            app.Run();
+        }
+    }
+}
---
+++ samples/Todo/AppShell.Samples.Todo.Mobile/Views/TodoPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppShell.Samples.Todo.Mobile.Views
+{
+    [View(typeof(TodoViewModel))]
+    public partial class TodoPage : ContentPage
+    {
+        public TodoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

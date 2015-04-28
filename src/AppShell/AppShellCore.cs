8b5b525aa85947b1145294c04c9dadc28bc41f8d
Christian Schwarz <christian.schwarz@refactory.at>
2015-04-28 17:51:41 +0200

    Start and stop configured plugins in AppShellCore

--- src/AppShell/AppShellCore.cs
+++ src/AppShell/AppShellCore.cs
@@ -1,19 +1,27 @@
 ﻿using SimpleInjector;
+using System.Collections.Generic;
 
 namespace AppShell
 {
     public class AppShellCore
     {
         public static Container Container { get; private set; }
-                
+
+        protected List<IPlugin> plugins;
+
         static AppShellCore()
         {
             Container = new Container();
         }
 
+        public AppShellCore()
+        {
+            plugins = new List<IPlugin>();
+        }
+
         public virtual void Configure()
         {
-            Container.RegisterSingle<IServiceDispatcher, ServiceDispatcher>(); 
+            Container.RegisterSingle<IServiceDispatcher, ServiceDispatcher>();
             Container.RegisterSingle<IViewModelFactory, ViewModelFactory>();
         }
 
@@ -21,10 +29,20 @@ namespace AppShell
         {
             Container.GetInstance<IViewFactory>().Initialize();
             Container.GetInstance<IServiceDispatcher>().Initialize();
+
+            foreach (TypeConfiguration pluginConfiguration in Container.GetInstance<IShellConfigurationProvider>().GetPlugins())
+            {
+                IPlugin plugin = Container.GetInstance(pluginConfiguration.Type) as IPlugin;
+                plugin.Start();
+
+                plugins.Add(plugin);
+            }
         }
-        
+
         public virtual void Shutdown()
         {
+            foreach (IPlugin plugin in plugins)
+                plugin.Stop();
         }
     }
 }

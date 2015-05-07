547c08254c1a744950ac6787ba6ccb6a931a899b
Christian Schwarz <christian.schwarz@refactory.at>
2015-05-07 22:22:21 +0200

    Added support for converting arrays

--- src/AppShell/Extensions/ObjectExtensions.cs
+++ src/AppShell/Extensions/ObjectExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System.Collections;
+using System.Linq;
+using System.Reflection;
 
 namespace System
 {
@@ -16,6 +18,22 @@ namespace System
 
             if (conversionTypeInfo.IsAssignableFrom(valueTypeInfo))
                 return value;
+
+            if (valueType.IsArray && conversionType.IsArray)
+            {
+                Type elementConversionType = conversionType.GetElementType();
+                var valueArray = (value as Array);
+
+                if (valueArray != null)
+                {
+                    Array array = Array.CreateInstance(elementConversionType, valueArray.Length);
+
+                    for (int i = 0; i < valueArray.Length; i++)
+                        array.SetValue(valueArray.GetValue(i).ChangeType(elementConversionType), i);
+
+                    return array;
+                }
+            }
 
             return Convert.ChangeType(value, conversionType);
         }
---
+++ test/AppShell.Tests/Extensions/ObjectExtensionsTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AppShell.Tests
+{
+    public class ObjectExtensionsTest
+    {
+        [Fact]
+        public void ChangeType_StringToString()
+        {
+            Assert.Equal("1", "1".ChangeType(typeof(string)));
+        }
+
+        [Fact]
+        public void ChangeType_IntToInt()
+        {
+            Assert.Equal(1, 1.ChangeType(typeof(int)));
+        }
+
+        [Fact]
+        public void ChangeType_IntToString()
+        {
+            Assert.Equal("1", 1.ChangeType(typeof(string)));
+        }
+
+        [Fact]
+        public void ChangeType_StringToInt()
+        {
+            Assert.Equal(1, "1".ChangeType(typeof(int)));
+        }
+
+        [Fact]
+        public void ChangeType_ObjectArrayOfStringsToStringArray()
+        {
+            Assert.Equal(new string[] { "1", "2", "3" }, new object[] { "1", "2", "3" }.ChangeType(typeof(string[])));
+        }
+
+        [Fact]
+        public void ChangeType_ObjectArrayOfIntsToStringArray()
+        {
+            Assert.Equal(new string[] { "1", "2", "3" }, new object[] { 1, 2, 3 }.ChangeType(typeof(string[])));
+        }
+
+        [Fact(Skip = "NotImplemented")]
+        public void ChangeType_ObjectArrayOfStringsToStringEnumerable()
+        {
+            Assert.Equal(new List<string>() { "1", "2", "3" }, new object[] { "1", "2", "3" }.ChangeType(typeof(IEnumerable<string>)));
+        }
+
+        [Fact(Skip = "NotImplemented")]
+        public void ChangeType_ObjectArrayOfIntsToStringEnumerable()
+        {
+            Assert.Equal(new List<string>() { "1", "2", "3" }, new object[] { 1, 2, 3 }.ChangeType(typeof(IEnumerable<string>)));
+        }
+    }
+}

da667df9b9f0aa4aa8d070ec0f302210b6c416ca
Christian Schwarz <christian.schwarz@refactory.at>
2015-07-30 10:59:00 +0200

    Added AssemblyInformationalVersion

--- src/AppShell.Desktop/Properties/AssemblyInfo.cs
+++ src/AppShell.Desktop/Properties/AssemblyInfo.cs
@@ -54,5 +54,6 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 [assembly: AppShellResource]
--- src/AppShell.Mobile/Properties/AssemblyInfo.cs
+++ src/AppShell.Mobile/Properties/AssemblyInfo.cs
@@ -29,5 +29,6 @@ using AppShell;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 [assembly: AppShellResource]

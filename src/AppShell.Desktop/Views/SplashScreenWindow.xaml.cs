32ae65f6ebac7330bf7a9bfb6457a694525333f2
Christian Schwarz <christian.schwarz@refactory.at>
2015-09-04 14:04:08 +0200

    Recenter splash screens after resize

--- src/AppShell.Desktop/Views/SplashScreenWindow.xaml.cs
+++ src/AppShell.Desktop/Views/SplashScreenWindow.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace AppShell.Desktop
 {
@@ -45,6 +34,14 @@ namespace AppShell.Desktop
             }
         }
 
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            
+            Top += (sizeInfo.PreviousSize.Height - sizeInfo.NewSize.Height) / 2;
+            Left += (sizeInfo.PreviousSize.Width - sizeInfo.NewSize.Width) / 2;
+        }
+        
         protected override void OnActivated(EventArgs e)
         {
             base.OnActivated(e);

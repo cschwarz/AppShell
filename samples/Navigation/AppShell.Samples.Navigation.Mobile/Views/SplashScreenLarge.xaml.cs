caf110d5b8c4a223305836203209ce35c09f859b
Christian Schwarz <christian.schwarz@refactory.at>
2015-09-29 19:34:50 +0200

    Added splash screen views to mobile navigation sample

---
+++ samples/Navigation/AppShell.Samples.Navigation.Mobile/Views/SplashScreenLarge.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppShell.Samples.Navigation.Mobile.Views
+{
+    [View(typeof(SplashScreenLargeViewModel))]
+    public partial class SplashScreenLarge : ContentView
+    {
+        public SplashScreenLarge()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ samples/Navigation/AppShell.Samples.Navigation.Mobile/Views/SplashScreenSmall.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppShell.Samples.Navigation.Mobile.Views
+{
+    [View(typeof(SplashScreenSmallViewModel))]
+    public partial class SplashScreenSmall : ContentView
+    {
+        public SplashScreenSmall()
+        {
+            InitializeComponent();
+        }
+    }
+}

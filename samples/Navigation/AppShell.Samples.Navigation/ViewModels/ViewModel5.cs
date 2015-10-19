71467a4d9e1650b35bd3cbaf867e23c406a906d0
Christian Schwarz <christian.schwarz@refactory.at>
2015-10-19 18:24:41 +0200

    Added close button to navigation sample

--- samples/Navigation/AppShell.Samples.Navigation/ViewModels/ViewModel5.cs
+++ samples/Navigation/AppShell.Samples.Navigation/ViewModels/ViewModel5.cs
@@ -2,9 +2,22 @@
 {
     public class ViewModel5 : ViewModel
     {
-        public ViewModel5()
+        public Command CloseCommand { get; private set; }
+
+        private IServiceDispatcher serviceDispatcher;
+
+        public ViewModel5(IServiceDispatcher serviceDispatcher)
         {
+            this.serviceDispatcher = serviceDispatcher;
+
             Title = "ViewModel5";
+
+            CloseCommand = new Command(Close);
+        }
+
+        public void Close()
+        {
+            serviceDispatcher.Dispatch<INavigationService>("InlineStackShell", n => n.Pop());
         }
     }
 }

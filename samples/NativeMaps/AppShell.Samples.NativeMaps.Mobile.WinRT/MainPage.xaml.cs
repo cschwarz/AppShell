8206b9df3b3144727fadf3a6f7bf5a6d140234fa
Christian Schwarz <christian.schwarz@refactory.at>
2015-11-21 23:06:41 +0100

    Added missing sample projects

---
+++ samples/NativeMaps/AppShell.Samples.NativeMaps.Mobile.WinRT/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AppShell.Mobile;
+using AppShell.Mobile.WinRT;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace AppShell.Samples.NativeMaps.Mobile.WinRT
+{
+    public class NativeMapsShellPage : ShellPage<ShellApplication<NativeMapsShellCore>>
+    {
+    }
+
+    public sealed partial class MainPage : NativeMapsShellPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Init();
+        }
+    }
+}
---
+++ samples/ServiceDispatcher/AppShell.Samples.ServiceDispatcher.Mobile.WinRT/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppShell.Mobile;
+using AppShell.Mobile.WinRT;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace AppShell.Samples.ServiceDispatcher.Mobile.WinRT
+{
+    public class ServiceDispatcherShellPage : ShellPage<ShellApplication<ServiceDispatcherShellCore>>
+    {
+    }
+
+    public sealed partial class MainPage : ServiceDispatcherShellPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Init();
+        }
+    }
+}

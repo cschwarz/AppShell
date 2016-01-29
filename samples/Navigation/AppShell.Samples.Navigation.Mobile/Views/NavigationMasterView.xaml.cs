c94e9cdc912a7eb1008022a35ba8a90462cad87d
Christian Schwarz <christian.schwarz@refactory.at>
2016-01-29 19:22:56 +0100

    Added support for a header content to MasterView

---
+++ samples/Navigation/AppShell.Samples.Navigation.Mobile/Views/NavigationMasterView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using AppShell;
+using Xamarin.Forms;
+
+namespace AppShell.Samples.Navigation.Mobile.Views
+{
+    [View(typeof(NavigationMasterViewModel))]
+    public partial class NavigationMasterView : ContentPage
+    {
+        public NavigationMasterView()
+        {
+            InitializeComponent();
+        }
+    }
+}

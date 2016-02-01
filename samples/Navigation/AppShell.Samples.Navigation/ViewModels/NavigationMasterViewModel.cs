c370507ee2ee3114aaa3f45383e1b429b8b126f9
Christian Schwarz <christian.schwarz@refactory.at>
2016-02-01 19:05:38 +0100

    Improved master view

--- samples/Navigation/AppShell.Samples.Navigation/ViewModels/NavigationMasterViewModel.cs
+++ samples/Navigation/AppShell.Samples.Navigation/ViewModels/NavigationMasterViewModel.cs
@@ -5,10 +5,10 @@
         public NavigationMasterViewModel(IServiceDispatcher serviceDispatcher)
             : base(serviceDispatcher)
         {
-            Items.Add(new ViewModelMenuItem("View 3", new TypeConfiguration(typeof(ViewModel3))));
-            Items.Add(new ViewModelMenuItem("View 4", new TypeConfiguration(typeof(ViewModel4))));
-            Items.Add(new ViewModelMenuItem("View 5", new TypeConfiguration(typeof(ViewModel5))));
-            Items.Add(new CommandMenuItem("Back", new Command(Back)));
+            Items.Add(new ViewModelMenuItem("View 3", new TypeConfiguration(typeof(ViewModel3))) { Icon = "Icon16.png" });
+            Items.Add(new ViewModelMenuItem("View 4", new TypeConfiguration(typeof(ViewModel4))) { Icon = "Icon24.png" });
+            Items.Add(new ViewModelMenuItem("View 5", new TypeConfiguration(typeof(ViewModel5))) { Icon = "Icon32.png" });
+            Items.Add(new CommandMenuItem("Back", new Command(Back)) { Icon = "Icon48.png" });
         }
 
         private void Back()
